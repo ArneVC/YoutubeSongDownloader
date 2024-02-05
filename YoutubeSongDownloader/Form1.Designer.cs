2a380797b93585ff061a6c4d2544a754e7c5a904
Arne <arnev.c.23@gmail.com>
2024-02-05 19:50:45 +0100

    add controls for downloading using url or song name

--- YoutubeSongDownloader/Form1.Designer.cs
+++ YoutubeSongDownloader/Form1.Designer.cs
@@ -29,13 +29,74 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            LabelUrl = new Label();
+            ButtonDownload = new Button();
+            RadioButtonUrl = new RadioButton();
+            RadioButtonSongName = new RadioButton();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 27);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(776, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // LabelUrl
+            // 
+            LabelUrl.AutoSize = true;
+            LabelUrl.Location = new Point(12, 9);
+            LabelUrl.Name = "LabelUrl";
+            LabelUrl.Size = new Size(69, 15);
+            LabelUrl.TabIndex = 1;
+            LabelUrl.Text = "Youtube Url";
+            LabelUrl.Click += label1_Click;
+            // 
+            // ButtonDownload
+            // 
+            ButtonDownload.Location = new Point(12, 56);
+            ButtonDownload.Name = "ButtonDownload";
+            ButtonDownload.Size = new Size(776, 23);
+            ButtonDownload.TabIndex = 2;
+            ButtonDownload.Text = "Download";
+            ButtonDownload.UseVisualStyleBackColor = true;
+            ButtonDownload.Click += ButtonDownload_Click;
+            // 
+            // RadioButtonUrl
+            // 
+            RadioButtonUrl.AutoSize = true;
+            RadioButtonUrl.Location = new Point(673, 5);
+            RadioButtonUrl.Name = "RadioButtonUrl";
+            RadioButtonUrl.Size = new Size(115, 19);
+            RadioButtonUrl.TabIndex = 3;
+            RadioButtonUrl.TabStop = true;
+            RadioButtonUrl.Text = "Use Youtube URL";
+            RadioButtonUrl.UseVisualStyleBackColor = true;
+            // 
+            // RadioButtonSongName
+            // 
+            RadioButtonSongName.AutoSize = true;
+            RadioButtonSongName.Location = new Point(558, 5);
+            RadioButtonSongName.Name = "RadioButtonSongName";
+            RadioButtonSongName.Size = new Size(109, 19);
+            RadioButtonSongName.TabIndex = 4;
+            RadioButtonSongName.TabStop = true;
+            RadioButtonSongName.Text = "Use Song Name";
+            RadioButtonSongName.UseVisualStyleBackColor = true;
+            RadioButtonSongName.CheckedChanged += RadioButtonSongName_CheckedChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(RadioButtonSongName);
+            Controls.Add(RadioButtonUrl);
+            Controls.Add(ButtonDownload);
+            Controls.Add(LabelUrl);
+            Controls.Add(textBox1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
@@ -43,8 +104,15 @@
             Text = "Youtube Song Downloader";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label LabelUrl;
+        private Button ButtonDownload;
+        private RadioButton RadioButtonUrl;
+        private RadioButton RadioButtonSongName;
     }
 }
--- YoutubeSongDownloader/Form1.cs
+++ YoutubeSongDownloader/Form1.cs
@@ -1,7 +1,10 @@
+using System.Diagnostics;
+
 namespace YoutubeSongDownloader
 {
     public partial class Form1 : Form
     {
+        private String userInput = "";
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +12,35 @@ namespace YoutubeSongDownloader
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            RadioButtonSongName.Checked = true;
+            LabelUrl.Text = "Song Name:";
+        }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            userInput = textBox1.Text;
+        }
+
+        private void ButtonDownload_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButtonSongName_CheckedChanged(object sender, EventArgs e)
+        {
+            if(RadioButtonSongName.Checked)
+            {
+                LabelUrl.Text = "Song Name:";
+            }
+            else
+            {
+                LabelUrl.Text = "Youtube Url:";
+            }
         }
     }
 }
