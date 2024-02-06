78e2821fc835c0e930cc0e1511f2f923cb9a3c2d
Arne <arnev.c.23@gmail.com>
2024-02-06 23:47:19 +0100

    add abstraction to work with file

---
+++ YoutubeSongDownloader/Data/FileBytesAbstraction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeSongDownloader.Data
+{
+    public class FileBytesAbstraction : TagLib.File.IFileAbstraction
+    {
+        public FileBytesAbstraction(string name, byte[] bytes)
+        {
+            Name = name;
+
+            var stream = new MemoryStream(bytes);
+            ReadStream = stream;
+            WriteStream = stream;
+        }
+        public void CloseStream(Stream stream)
+        {
+            stream.Dispose();
+        }
+        public string Name { get; private set; }
+        public Stream ReadStream { get; private set; }
+        public Stream WriteStream { get; private set; }
+    }
+}
