ff4a3c51801ba57e7797d5b14d1a40472ffe6fdd
Arne <arnev.c.23@gmail.com>
2024-03-06 00:07:34 +0100

    remove redundant variable

--- YoutubeSongDownloader/SongDownloader.cs
+++ YoutubeSongDownloader/SongDownloader.cs
@@ -58,7 +58,7 @@ namespace YoutubeSongDownloader
                     }
                 }
             }
-            catch (Exception ex)
+            catch ()
             {
                 return null;
             }
