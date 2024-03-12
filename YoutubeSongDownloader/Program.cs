575f3914bafba71cff0d3642bc29c7b225a251cf
Arne <arnev.c.23@gmail.com>
2024-03-12 22:46:25 +0100

    setup config file at start of program

--- YoutubeSongDownloader/Program.cs
+++ YoutubeSongDownloader/Program.cs
@@ -1,7 +1,10 @@
+using System.Diagnostics;
+
 namespace YoutubeSongDownloader
 {
     internal static class Program
     {
+        private static string configFilePath = "./config.json";
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -10,8 +13,16 @@ namespace YoutubeSongDownloader
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            CreateConfigFileIfItDoesntExist();
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+        }
+        static void CreateConfigFileIfItDoesntExist()
+        {
+            if(!File.Exists(configFilePath))
+            {
+                File.Create("config.json").Dispose();
+            }
         }
     }
 }
