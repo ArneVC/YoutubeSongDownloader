0527ef999ed54bf55a804d92dd430b1d4a9e4e5d
Arne <arnev.c.23@gmail.com>
2024-03-12 22:48:44 +0100

    create class for storing program config

---
+++ YoutubeSongDownloader/Data/ProgramConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeSongDownloader.Data
+{
+    public class ProgramConfig
+    {
+        public string outputFolderPath { get; set; }
+        public override string ToString()
+        {
+            return "ProgramConfig(outputFolderPath" + outputFolderPath + ")";
+        }
+    }
+}
