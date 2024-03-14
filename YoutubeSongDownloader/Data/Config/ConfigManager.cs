794c5974f8ffa130456b20de689d41703e15364c
Arne <arnev.c.23@gmail.com>
2024-03-14 12:57:49 +0100

    move config logic into ConfigManager

--- YoutubeSongDownloader/Data/Config/ConfigManager.cs
+++ YoutubeSongDownloader/Data/Config/ConfigManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace YoutubeSongDownloader.Data.Config
 {
     public static class ConfigManager
     {
+        private static string configFilePath = "./config.json";
+        public static void CreateConfigFileIfItDoesntExist()
+        {
+            if (!File.Exists(configFilePath))
+            {
+                using (StreamWriter writer = File.CreateText(configFilePath))
+                {
+                    ProgramConfig defaultConfig = new ProgramConfig(KnownFolders.GetPath(KnownFolder.Downloads));
+                    string json = JsonConvert.SerializeObject(defaultConfig);
+                    writer.Write(json);
+                }
+            }
+        }
+        public static ProgramConfig ReadConfigFile()
+        {
+            string json = File.ReadAllText(configFilePath);
+            ProgramConfig savedConfig = JsonConvert.DeserializeObject<ProgramConfig>(json);
+            return savedConfig;
+        }
     }
 }
--- YoutubeSongDownloader/Program.cs
+++ YoutubeSongDownloader/Program.cs
@@ -7,15 +7,14 @@ namespace YoutubeSongDownloader
 {
     internal static class Program
     {
-        private static string configFilePath = "./config.json";
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            CreateConfigFileIfItDoesntExist();
-            ProgramConfig configToInit = ReadConfigFile();
+            ConfigManager.CreateConfigFileIfItDoesntExist();
+            ProgramConfig configToInit = ConfigManager.ReadConfigFile();
             if(configToInit != null)
             {
                 Debug.WriteLine(configToInit.ToString());
@@ -26,25 +25,6 @@ namespace YoutubeSongDownloader
             {
                 MessageBox.Show("Config file error!\nPlease fix or delete config file");
             }
-        }
-        //TODO: refactor into config utility class (changing config in other classes)
-        private static void CreateConfigFileIfItDoesntExist()
-        {
-            if(!File.Exists(configFilePath))
-            {
-                using (StreamWriter writer = File.CreateText(configFilePath))
-                {
-                    ProgramConfig defaultConfig = new ProgramConfig(KnownFolders.GetPath(KnownFolder.Downloads));
-                    string json = JsonConvert.SerializeObject(defaultConfig);
-                    writer.Write(json);
-                }
-            }
-        }
-        private static ProgramConfig ReadConfigFile()
-        {
-            string json = File.ReadAllText(configFilePath);
-            ProgramConfig savedConfig = JsonConvert.DeserializeObject<ProgramConfig>(json);
-            return savedConfig;
         }
     }
 }
