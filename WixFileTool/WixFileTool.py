71b0fc7240fea85c85b7415bf4bc8d92ae23f12a
Arne <arnev.c.23@gmail.com>
2024-03-16 17:13:58 +0100

    don't add directories to file path list

--- WixFileTool/WixFileTool.py
+++ WixFileTool/WixFileTool.py
@@ -3,5 +3,11 @@ import os
 sourcedirectory = input("define source directory: ")
 relativePathFromWixFileToBuildFolder = input("define relative path from wix folder to build folder: ") + "\\"
 files = os.listdir(sourcedirectory)
+filePaths = []
 for file in files:
-    print(relativePathFromWixFileToBuildFolder + file)
+    full_path = os.path.join(sourcedirectory, file)
+    if not os.path.isdir(full_path):
+        filePaths.append(relativePathFromWixFileToBuildFolder + file)
+        
+for filepath in filePaths:
+    print(filepath)
