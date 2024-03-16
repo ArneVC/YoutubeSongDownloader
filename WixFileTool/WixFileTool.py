5d528ac17a214921509229539d06a020dd175af4
Arne <arnev.c.23@gmail.com>
2024-03-16 17:24:05 +0100

    also get relative file paths for files in subfolders

--- WixFileTool/WixFileTool.py
+++ WixFileTool/WixFileTool.py
@@ -8,6 +8,12 @@ for file in files:
     full_path = os.path.join(sourcedirectory, file)
     if not os.path.isdir(full_path):
         filePaths.append(relativePathFromWixFileToBuildFolder + file)
+    else:
+        # assume subfolders don't have subfolders themselves as it's the case for this project
+        subDirectory = sourcedirectory + "\\" + file
+        subDirectoryFiles = os.listdir(subDirectory)
+        for subDirectoryFile in subDirectoryFiles:
+            filePaths.append(relativePathFromWixFileToBuildFolder + file + "\\" + subDirectoryFile)
         
 for filepath in filePaths:
     print(filepath)
